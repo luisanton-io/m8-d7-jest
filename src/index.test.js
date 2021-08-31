0653bef5195f8d480583e30aff9428bf75424421
Tiago Brandao <tiagofpbrandao@gmail.com>
2021-08-31 15:59:45 +0200

    Adds homework tests

--- src/index.test.js
+++ src/index.test.js
@@ -16,7 +16,7 @@ describe("Testing tests", () => {
 describe("Testing endpoints", () => {
   beforeAll(done => {
     mongoose
-      .connect(process.env.ATLAS_URL_PROD)
+      .connect(process.env.ATLAS_URL_DEV)
       .then(() => {
         console.log("Connected to Atlas!")
         done()
@@ -62,6 +62,42 @@ describe("Testing endpoints", () => {
 
     const getResponse = await request.get(`/products/${_id}`)
     expect(getResponse.body.name).toBe(validProduct.name)
+  })
+
+  it("should test that /products/:id returns 404 for a non-existent ID", async () => {
+    const response = await request.get("/products/612e2d855717a88a5ca19e0d")
+    expect(response.status).toBe(404)
+  })
+
+  it("should test that the /products endpoint is letting to successfully DELETE a product", async () => {
+    const response = await request.post("/products").send(validProduct)
+
+    expect(response.status).toBe(201)
+    expect(response.body.name).toBe(validProduct.name)
+
+    const _id = response.body._id
+
+    const deleteResponse = await request.delete(`/products/${_id}`)
+    expect(deleteResponse.status).toBe(204)
+  })
+
+  it("should test that /products/:id is updating items successfully", async () => {
+    const response = await request.post("/products").send(validProduct)
+
+    expect(response.status).toBe(201)
+    expect(response.body.name).toBe(validProduct.name)
+
+    const _id = response.body._id
+
+    const update = {
+      name: "Go Kart",
+      price: 2000,
+    }
+
+    const editResponse = await request.put(`/products/${_id}`).send(update)
+    expect(editResponse.status).not.toBe(404)
+    expect(editResponse.body.name).not.toBe(response.body.name)
+    expect(typeof editResponse.body.name).toBe("string")
   })
 
   afterAll(done => {
--- src/products/index.js
+++ src/products/index.js
@@ -1,35 +1,53 @@
-import express from 'express'
+import express from "express"
 import ProductModel from "./model.js"
 
-const productsRouter = express.Router();
+const productsRouter = express.Router()
 
-productsRouter.get('/', async (req, res) => {
-    const products = await ProductModel.find({})
-    res.status(200).send(products)
+productsRouter.get("/", async (req, res) => {
+  const products = await ProductModel.find({})
+  res.status(200).send(products)
 })
 
-productsRouter.get('/:id', async (req, res) => {
-    try {
-        const product = await ProductModel.findById(req.params.id)
-        res.status(200).send(product)
-    } catch (error) {
-        res.status(404).send()
-    }
+productsRouter.get("/:id", async (req, res) => {
+  try {
+    const product = await ProductModel.findById(req.params.id)
+    if (!product) res.status(404).send({ message: `Product with id ${req.params.id} not found.` })
+    res.status(200).send(product)
+  } catch (error) {
+    res.status(404).send()
+  }
 })
 
-productsRouter.post('/', async (req, res) => {
-    try {
-        const product = new ProductModel(req.body)
-        await product.save()
+productsRouter.post("/", async (req, res) => {
+  try {
+    const product = new ProductModel(req.body)
+    await product.save()
 
-        res.status(201).send(product)
-    } catch (error) {
-        console.log(error)
-        res.status(400).send()
-    }
-
+    res.status(201).send(product)
+  } catch (error) {
+    console.log(error)
+    res.status(400).send()
+  }
 })
 
+productsRouter.delete("/:id", async (req, res) => {
+  try {
+    const deleted = await ProductModel.findByIdAndDelete(req.params.id)
+    if (!deleted) res.status(404).send({ message: `Product with id ${req.params.id} not found.` })
+    res.status(204).send()
+  } catch (error) {
+    res.status(500).send({ message: "Something went wrong" })
+  }
+})
 
+productsRouter.put("/:id", async (req, res) => {
+  try {
+    const updated = await ProductModel.findByIdAndUpdate(req.params.id, { ...req.body }, { new: true })
+    if (!updated) res.status(404).send({ message: `Product with id ${req.params.id} not found.` })
+    res.send(updated)
+  } catch (error) {
+    res.status(500).send({ message: "Something went wrong" })
+  }
+})
 
 export default productsRouter
