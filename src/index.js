c27b292a379379981173529666aacbc4ecafe283
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-08-31 14:47:23 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 
 const port = process.env.PORT || 3030
 
-mongoose.connect(`${process.env.ATLAS_URL}/production`, () => {
+mongoose.connect(`${process.env.ATLAS_PRODUCTION_URL}`, () => {
     console.log("Connected to Atlas!")
     app.listen(port, () => {
         console.log("Server listening on port " + port)
