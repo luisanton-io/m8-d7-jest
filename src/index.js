ab7d29005e85bdca2f684f7a20c051228c12b016
Tiago Brandao <tiagofpbrandao@gmail.com>
2021-08-31 15:17:39 +0200

    Fixes db connection

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import app from "./app.js"
 import mongoose from "mongoose"
 
-const port = process.env.PORT || 3030
+const port = process.env.PORT || 3001
 
-mongoose.connect(`${process.env.ATLAS_URL}/production`, () => {
+mongoose
+  .connect(process.env.ATLAS_URL_PROD, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log("Connected to Atlas!")
     app.listen(port, () => {
-        console.log("Server listening on port " + port)
+      console.log("Server listening on port " + port)
     })
-})
+  })
+  .catch(err => console.log(err))
--- src/index.test.js
+++ src/index.test.js
@@ -7,69 +7,67 @@ dotenv.config()
 
 const request = supertest(app)
 
-
 describe("Testing tests", () => {
-
-    it("should test that true is true", () => {
-        expect(true).toBe(true)
-    })
-
+  it("should test that true is true", () => {
+    expect(true).toBe(true)
+  })
 })
 
 describe("Testing endpoints", () => {
+  beforeAll(done => {
+    mongoose
+      .connect(process.env.ATLAS_URL_PROD)
+      .then(() => {
+        console.log("Connected to Atlas!")
+        done()
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
 
-    beforeAll(done => {
-        mongoose.connect(`${process.env.ATLAS_URL}/test`, () => {
-            console.log("Connected to Atlas!")
-            done()
-        })
+  it("should test that the /test endpoint is returning 200 and a success message", async () => {
+    const response = await request.get("/test")
+
+    expect(response.status).toBe(200)
+    expect(response.body.message).toBe("Test success!")
+  })
+
+  it("should test that the /products endpoint is returning a list of products", async () => {
+    const response = await request.get("/products")
+
+    expect(response.status).toBe(200)
+    expect(response.body.length).toBeDefined()
+  })
+
+  const validProduct = {
+    name: "iPhone",
+    price: 900,
+  }
+
+  it("should test that the /products endpoint is letting POST a new product", async () => {
+    const response = await request.post("/products").send(validProduct)
+
+    expect(response.status).toBe(201)
+    expect(response.body.name).toBe(validProduct.name)
+  })
+
+  it("should test that the /products endpoint is returning one product with the correct id", async () => {
+    const response = await request.post("/products").send(validProduct)
+
+    expect(response.status).toBe(201)
+    expect(response.body.name).toBe(validProduct.name)
+
+    const _id = response.body._id
+
+    const getResponse = await request.get(`/products/${_id}`)
+    expect(getResponse.body.name).toBe(validProduct.name)
+  })
+
+  afterAll(done => {
+    mongoose.connection.dropDatabase().then(() => {
+      mongoose.connection.close()
+      done()
     })
-
-    it("should test that the /test endpoint is returning 200 and a success message", async () => {
-        const response = await request.get("/test")
-
-        expect(response.status).toBe(200)
-        expect(response.body.message).toBe("Test success!")
-    })
-
-    it("should test that the /products endpoint is returning a list of products", async () => {
-
-        const response = await request.get("/products")
-
-        expect(response.status).toBe(200)
-        expect(response.body.length).toBeDefined()
-    })
-
-    const validProduct = {
-        name: "iPhone",
-        price: 900
-    }
-
-    it("should test that the /products endpoint is letting POST a new product", async () => {
-        const response = await request.post("/products").send(validProduct)
-
-        expect(response.status).toBe(201)
-        expect(response.body.name).toBe(validProduct.name)
-    })
-
-    it("should test that the /products endpoint is returning one product with the correct id", async () => {
-        const response = await request.post("/products").send(validProduct)
-
-        expect(response.status).toBe(201)
-        expect(response.body.name).toBe(validProduct.name)
-
-        const _id = response.body._id
-
-        const getResponse = await request.get(`/products/${_id}`)
-        expect(getResponse.body.name).toBe(validProduct.name)
-
-    })
-
-    afterAll(done => {
-        mongoose.connection.dropDatabase().then(() => {
-            mongoose.connection.close()
-            done()
-        })
-    })
-
+  })
 })
